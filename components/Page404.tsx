98349f74574e3f7c84ed2d5b9220900b34d6d724
Oussama khouya <ouskhouya@gmail.com>
2025-01-25 01:33:14 +0100

    change site.config.ts + favicon + remove githubShareButton +

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -4,7 +4,7 @@ import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
 export function Page404({ site, pageId, error }: types.PageProps) {
-  const title = site?.name || 'Notion Page Not Found'
+  const title = site?.name || 'Page Not Found'
 
   return (
     <>
--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '18570bbaeaf380778b2dee2baaed75de',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'SDIA SPHERE',
+  domain: 'sdia-sphere.site',
+  author: 'Oussama Khouya',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Everything you need to succeed in your master\'s degree starts here.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: 'ouskhouya',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -42,24 +42,26 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    // '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+    '/about': '18570bbaeaf381299370d4cbc8bdb6a1',
+    '/contact': '18570bbaeaf38161b40dc263b57c3e17',
+    '/S1M4-ROO': 'm114--recherche-oprationnelle-et-optimisation-18570bbaeaf381329812f5df5dae9f19'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '18570bbaeaf381299370d4cbc8bdb6a1'
+    },
+    {
+      title: 'Contact',
+      pageId: '18570bbaeaf38161b40dc263b57c3e17'
+    }
+  ]
 })
